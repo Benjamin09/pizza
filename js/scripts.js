0bba259eea3c5aad30c07e42b14ef94abcd417d1
Cory Spitzer <coryspitzer@gmail.com>
2015-06-05 13:51:27 -0700

    Calculate the cost of a Pizza

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,10 @@ var Pizza = function(toppings, howBig, quantity) {
   this.quantity = quantity;
 };
 
+Pizza.prototype.cost = function() {
+  return 10 * this.quantity;
+};
+
 var Order = function(pizzas) {
   var cost = 0;
   pizzas.forEach(function(pizza) {
--- spec/specs.js
+++ spec/specs.js
@@ -4,10 +4,10 @@ describe("Pizza", function() {
     expect([pie.toppings, pie.howBig, pie.quantity]).to.eql(['nada', 'big', 2]);
   });
 
-  // it("calculates the cost of the order", function() {
-  //   var pie = new Pizza('nada', 'big', 2);
-  //   expect()
-  // });
+  it("calculates the cost of the pizza", function() {
+    var pie = new Pizza('nada', 'big', 2);
+    expect(pie.cost()).to.equal(20)
+  });
 });
 
 describe("Order", function() {
@@ -16,4 +16,10 @@ describe("Order", function() {
     var order = new Order([pie]);
     expect(order.pizzas).to.eql([pie]);
   });
+
+  // it("calculates the cost of an Order", function() {
+  //   var pie = new Pizza('nada', 'big', 2);
+  //   var order = new Order([pie]);
+  //   expect(order.cost).to.equal(2);
+  // });
 });

3757979d1cd83a206a47f2bce37368f364d657d8
Cory Spitzer <coryspitzer@gmail.com>
2015-06-05 15:02:24 -0700

    Test that an Order handles the costs of different pies

--- spec/specs.js
+++ spec/specs.js
@@ -17,9 +17,23 @@ describe("Order", function() {
     expect(order.pizzas).to.eql([pie]);
   });
 
+  it("initializes an Order with different types of pies", function() {
+    var pie = new Pizza('nada', 'big', 2);
+    var pie2 = new Pizza('pepperoni', 'medium', 1);
+    var order = new Order([pie, pie2]);
+    expect(order.pizzas).to.eql([pie, pie2]);
+  });
+
   it("calculates the cost of an Order", function() {
     var pie = new Pizza('nada', 'big', 2);
     var order = new Order([pie]);
     expect(order.cost).to.equal(20);
+  });
+
+  it("calculates the cost of an Order with different types of pies", function() {
+    var pie = new Pizza('nada', 'big', 2);
+    var pie2 = new Pizza('pepperoni', 'medium', 1);
+    var order = new Order([pie, pie2]);
+    expect(order.cost).to.equal(30);
   });
 });
